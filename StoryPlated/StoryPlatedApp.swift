fe201ab069364fe6fbd6cb7922c2b8171b9e34db
leonzh0u <leonzhou12588@hotmail.com>
2025-05-31 18:18:21 -0400

    Initial Commit

---
+++ StoryPlated/StoryPlatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryPlatedApp.swift
+//  StoryPlated
+//
+//  Created by Liyang Zhou on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct StoryPlatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryPlatedTests/StoryPlatedTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryPlatedTests.swift
+//  StoryPlatedTests
+//
+//  Created by Liyang Zhou on 5/31/25.
+//
+
+import Testing
+@testable import StoryPlated
+
+struct StoryPlatedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
