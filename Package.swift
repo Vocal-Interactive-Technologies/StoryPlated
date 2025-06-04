3c5a36b633ad82342d2e599330f70dc511e82aec
leonzh0u <leonzhou12588@hotmail.com>
2025-06-03 21:44:05 -0400

    pass workflow
    Signed-off-by: leonzh0u <leonzhou12588@hotmail.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "StoryPlated",
-            path: "/StoryPlated"
+            path: "StoryPlated"
         )
     ]
 )
